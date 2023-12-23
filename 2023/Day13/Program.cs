8f2b3d240ce9af71ad8d6b3aa09afd6cc319b6cc
Stefan Kert <stefan.kert@sbg.at>
2023-12-23 12:11:43 +0100

    Added Day 13 and Day14

---
+++ 2023/Day13/Program.cs
@@ -0,0 +1,88 @@
+﻿var patterns = ParseMaps(File.ReadAllText("input.txt"));
+var matchNum = 0;
+foreach (var pattern in patterns)
+{
+    var (dim, loc) = FindReflection(pattern,1);
+    var multiply = !dim ? 100 : 1;
+    matchNum += loc * multiply;
+}
+
+Console.WriteLine("Amount of maps: " + matchNum);
+
+static (bool, int) FindReflection(char[,] pattern, int smudges)
+{
+    var xl = pattern.GetLength(0);
+    var yl = pattern.GetLength(1);
+
+    for (int x = 1; x < xl; x++)
+    {
+        if (FoundVerticalReflection(pattern, x, smudges))
+            return (true, x);
+    }
+
+    for (int y = 1; y < yl; y++)
+    {
+        if (FoundHorizontalReflection(pattern, y, smudges))
+            return (false, y);
+    }
+
+    throw new NotImplementedException();
+}
+
+static bool FoundVerticalReflection(char[,] pattern, int x, int smudges)
+{
+    var xl = pattern.GetLength(0);
+    var yl = pattern.GetLength(1);
+    var dl = DLength(x, xl);
+    int sCount = 0;
+    for (int d = 0; d < dl; d++)
+    {
+        for (int y = 0; y < yl; y++)
+        {
+            if (pattern[x - 1 - d, y] != pattern[x + d, y])
+                sCount++;
+        }
+    }
+
+    return smudges == sCount;
+}
+
+static bool FoundHorizontalReflection(char[,] pattern, int y, int smudges)
+{
+    var xl = pattern.GetLength(0);
+    var yl = pattern.GetLength(1);
+    var dl = DLength(y, yl);
+    int sCount = 0;
+    for (int d = 0; d < dl; d++)
+    {
+        for (int x = 0; x < xl; x++)
+        {
+            if (pattern[x, y - 1 - d] != pattern[x, y + d])
+                sCount++;
+        }
+    }
+
+    return smudges == sCount;
+}
+
+static int DLength(int point, int length) => Math.Min(point, length - point);
+
+static List<char[,]> ParseMaps(string input) => input.Split($"\n\n").Select(ParseMap).ToList();
+
+//static List<char[,]> ParseMaps(string input) => input.Split($"{Environment.NewLine}{Environment.NewLine}").Select(ParseMap).ToList();
+
+static char[,] ParseMap(string input)
+{
+    var rows = input.Split("\n", StringSplitOptions.TrimEntries);
+    int yl = rows.Length;
+    int xl = rows.First().Length;
+    char[,] arr = new char[xl, yl];
+    for (int y = 0; y < yl; y++)
+    {
+        for (int x = 0; x < xl; x++)
+        {
+            arr[x, y] = rows[y][x];
+        }
+    }
+    return arr;
+}
---
+++ 2023/Day14/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Data;
+using Map = char[][];
+
+Console.WriteLine("Solution 1: {0}", Measure(Tilt(Parse(File.ReadAllText("input.txt")))));
+Console.WriteLine("Solution 2: {0}", Measure(Iterate(Parse(File.ReadAllText("input.txt")), 1_000_000_000)));
+
+static Map Iterate(Map map, int count)
+{
+    var history = new List<string>();
+    while (count > 0)
+    {
+        for (var i = 0; i < 4; i++)
+        {
+            map = Turn90DegreeClockwise(Tilt(map));
+        }
+        count--;
+
+        var mapString = string.Join("\n", map.Select(l => new string(l)));
+        var idx = history.IndexOf(mapString);
+        if (idx < 0)
+        {
+            history.Add(mapString);
+        }
+        else
+        {
+            var loopLength = history.Count - idx;
+            var remainder = count % loopLength;
+            return Parse(history[idx + remainder]);
+        }
+    }
+    return map;
+}
+
+static Map Tilt(Map map)
+{
+    for (int icol = 0; icol < map[0].Length; icol++)
+    {
+        var irowT = 0;
+        for (int irow = 0; irow < map.Length; irow++)
+        {
+            if (map[irow][icol] == '#')
+            {
+                irowT = irow + 1;
+                continue;
+            }
+
+            if (map[irow][icol] == 'O')
+            {
+                map[irow][icol] = '.';
+                map[irowT][icol] = 'O';
+                irowT++;
+            }
+        }
+    }
+    return map;
+}
+
+static Map Turn90DegreeClockwise(Map src)
+{
+    var dst = new char[src.Length][];
+    for (var irow = 0; irow < src[0].Length; irow++)
+    {
+        dst[irow] = new char[src[0].Length];
+        for (var icol = 0; icol < src.Length; icol++)
+        {
+            dst[irow][icol] = src[src.Length - icol - 1][irow];
+        }
+    }
+    return dst;
+}
+
+static int Measure(Map map) => map.Select((row, irow) => (map.Length - irow) * row.Count(ch => ch == 'O')).Sum();
+
+static Map Parse(string input) => input.Split('\n').Select(x => x.ToCharArray()).ToArray(); 
