f178d208ee1c0872130f5cad2a8f8d91f67c59ac
Stefan Kert <stefan.kert@sbg.at>
2023-12-23 19:15:11 +0100

    added day 16 draft

--- 2023/Day16/Program.cs
+++ 2023/Day16/Program.cs
@@ -1,145 +1,138 @@
-﻿using Map = char[][];
+﻿using Map = (char symbol, int energized)[][];
 
-var map = Parse(File.ReadAllText("demo.txt"));
+var map = Parse(File.ReadAllText("input.txt"));
+Data(map, 0, 0, MovingDirections.Right);
 
-var currentRow = 0;
-var currentCol = 0;
-var walkingDirectionHorizontal = MovingDirections.Right;
-var curVal = map[currentRow][currentCol];
-map[currentRow][currentCol] = '#';
-
-Data(map, 0, 0, MovingDirections.Right, true);
+Console.WriteLine("Solution 1 " + CalcEnergized(map));
 
 
-static void Data(Map map, int startRow, int startColumn, MovingDirections direction, bool printLayout = false)
+static void Data(Map map, int startRow, int startColumn, MovingDirections direction)
 {
+    if (!CheckBounds(map, startRow, startColumn))
+    {
+        return;
+    }
+
     var walkingDirectionHorizontal = direction;
     var currentRow = startRow;
     var currentCol = startColumn;
-    var curVal = map[currentRow][currentCol];
-    map[currentRow][currentCol] = '#';
+    var alreadyEnergized = false;
+    if (map[currentRow][currentCol].energized > 1000)
+    {
+        return;
+    }
+    map[currentRow][currentCol].energized++;
+
     while (true)
     {
-        if (curVal == '.')
+        var curVal = map[currentRow][currentCol];
+        switch (curVal.symbol)
         {
-            switch (walkingDirectionHorizontal)
-            {
-                case MovingDirections.Left:
-                    currentCol--;
-                    break;
-                case MovingDirections.Right:
-                    currentCol++;
-                    break;
-                case MovingDirections.Up:
-                    currentRow--;
-                    break;
-                case MovingDirections.Down:
-                    currentRow++;
-                    break;
-            }
-        }
-
-        if (curVal == '|')
-        {
-            switch (walkingDirectionHorizontal)
-            {
-                case MovingDirections.Left:
-                    Data(map, currentRow--, currentCol, MovingDirections.Up);
-                    Data(map, currentRow++, currentCol, MovingDirections.Down);
-                    break;
-                case MovingDirections.Right:
-                    Data(map, currentRow--, currentCol, MovingDirections.Up);
-                    Data(map, currentRow++, currentCol, MovingDirections.Down);
-                    break;
-                case MovingDirections.Up:
-                    currentRow--;
-                    break;
-                case MovingDirections.Down:
-                    currentRow++;
-                    break;
-            }
-        }
-
-
-        if (curVal == '-')
-        {
-            switch (walkingDirectionHorizontal)
-            {
-                case MovingDirections.Left:
-                    currentCol--;
-                    break;
-                case MovingDirections.Right:
-                    currentCol++;
-                    break;
-                case MovingDirections.Up:
-                    Data(map, currentRow, currentCol++, MovingDirections.Right);
-                    Data(map, currentRow, currentCol--, MovingDirections.Left);
-                    break;
-                case MovingDirections.Down:
-                    Data(map, currentRow, currentCol++, MovingDirections.Right);
-                    Data(map, currentRow, currentCol--, MovingDirections.Left);
-                    break;
-            }
-        }
-
-
-        if (curVal == '/')
-        {
-            switch (walkingDirectionHorizontal)
-            {
-                case MovingDirections.Left:
-                    walkingDirectionHorizontal = MovingDirections.Down;
-                    currentRow++;
-                    break;
-                case MovingDirections.Right:
-                    walkingDirectionHorizontal = MovingDirections.Up;
-                    currentRow--;
-                    break;
-                case MovingDirections.Up:
-                    walkingDirectionHorizontal = MovingDirections.Right;
-                    currentCol++;
-                    break;
-                case MovingDirections.Down:
-                    walkingDirectionHorizontal = MovingDirections.Left;
-                    currentCol--;
-                    break;
-            }
-        }
-
-        if (curVal == '\\')
-        {
-            switch (walkingDirectionHorizontal)
-            {
-                case MovingDirections.Left:
-                    walkingDirectionHorizontal = MovingDirections.Up;
-                    currentRow--;
-                    break;
-                case MovingDirections.Right:
-                    walkingDirectionHorizontal = MovingDirections.Down;
-                    currentRow++;
-                    break;
-                case MovingDirections.Up:
-                    walkingDirectionHorizontal = MovingDirections.Left;
-                    currentCol--;
-                    break;
-                case MovingDirections.Down:
-                    walkingDirectionHorizontal = MovingDirections.Right;
-                    currentCol++;
-                    break;
-            }
+            case '.':
+                switch (walkingDirectionHorizontal)
+                {
+                    case MovingDirections.Left:
+                        currentCol--;
+                        break;
+                    case MovingDirections.Right:
+                        currentCol++;
+                        break;
+                    case MovingDirections.Up:
+                        currentRow--;
+                        break;
+                    case MovingDirections.Down:
+                        currentRow++;
+                        break;
+                }
+                break;
+            case '|':
+                switch (walkingDirectionHorizontal)
+                {
+                    case MovingDirections.Left:
+                    case MovingDirections.Right:
+                        Data(map, currentRow - 1, currentCol, MovingDirections.Up);
+                        Data(map, currentRow + 1, currentCol, MovingDirections.Down);
+                        return;
+                    case MovingDirections.Up:
+                        currentRow--;
+                        break;
+                    case MovingDirections.Down:
+                        currentRow++;
+                        break;
+                }
+                break;
+            case '-':
+                switch (walkingDirectionHorizontal)
+                {
+                    case MovingDirections.Left:
+                        currentCol--;
+                        break;
+                    case MovingDirections.Right:
+                        currentCol++;
+                        break;
+                    case MovingDirections.Up:
+                    case MovingDirections.Down:
+                        Data(map, currentRow, currentCol + 1, MovingDirections.Right);
+                        Data(map, currentRow, currentCol - 1, MovingDirections.Left);
+                        return;
+                }
+                break;
+            case '/':
+                switch (walkingDirectionHorizontal)
+                {
+                    case MovingDirections.Left:
+                        walkingDirectionHorizontal = MovingDirections.Down;
+                        currentRow++;
+                        break;
+                    case MovingDirections.Right:
+                        walkingDirectionHorizontal = MovingDirections.Up;
+                        currentRow--;
+                        break;
+                    case MovingDirections.Up:
+                        walkingDirectionHorizontal = MovingDirections.Right;
+                        currentCol++;
+                        break;
+                    case MovingDirections.Down:
+                        walkingDirectionHorizontal = MovingDirections.Left;
+                        currentCol--;
+                        break;
+                }
+                break;
+            case '\\':
+                switch (walkingDirectionHorizontal)
+                {
+                    case MovingDirections.Left:
+                        walkingDirectionHorizontal = MovingDirections.Up;
+                        currentRow--;
+                        break;
+                    case MovingDirections.Right:
+                        walkingDirectionHorizontal = MovingDirections.Down;
+                        currentRow++;
+                        break;
+                    case MovingDirections.Up:
+                        walkingDirectionHorizontal = MovingDirections.Left;
+                        currentCol--;
+                        break;
+                    case MovingDirections.Down:
+                        walkingDirectionHorizontal = MovingDirections.Right;
+                        currentCol++;
+                        break;
+                }
+                break;
         }
 
         if (!CheckBounds(map, currentRow, currentCol))
         {
             break;
         }
-        curVal = map[currentRow][currentCol];
-        map[currentRow][currentCol] = '#';
 
-        if (printLayout)
+        if (map[currentRow][currentCol].energized > 0 && alreadyEnergized)
         {
-            PrintLayout(map);
+            return;
         }
+
+        map[currentRow][currentCol].energized++;
+        //PrintLayout(map);
     }
 }
 
@@ -167,18 +160,43 @@ static void PrintLayout(Map map)
     {
         for (var icol = 0; icol < map[0].Length; icol++)
         {
-            Console.Write(map[irow][icol]);
+            var value = map[irow][icol];
+            if (value.energized > 0)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write(value.symbol);
+            }
         }
         Console.WriteLine();
     }
 }
 
-static Map Parse(string input) => input.Split(Environment.NewLine).Select(x => x.ToCharArray()).ToArray();
+static int CalcEnergized(Map map)
+{
+    var sum = 0;
+    for (var irow = 0; irow < map.Length; irow++)
+    {
+        for (var icol = 0; icol < map[0].Length; icol++)
+        {
+            var value = map[irow][icol];
+            if (value.energized > 0)
+            {
+                sum++;
+            }
+        }
+    }
+    return sum;
+}
+
+static Map Parse(string input) => input.Split(Environment.NewLine).Select(x => x.ToCharArray()).ToArray().Select(x => x.Select(y => (y, 0)).ToArray()).ToArray();
 
 public enum MovingDirections
 {
-    Up,
-    Down,
-    Right,
-    Left
+    Up = 1,
+    Down = 2,
+    Right = 4,
+    Left = 8
 }
