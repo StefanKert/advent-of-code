631917f4a2a16da0fa8777a877ab038a5bdc5e85
Stefan Kert <stefan.kert@sbg.at>
2024-12-07 22:26:44 +0100

    adde dday 7

--- 2024/Day7/Program.cs
+++ 2024/Day7/Program.cs
@@ -1,9 +1,116 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 
-Console.WriteLine($"NullReferenceException message is: {new NullReferenceException().Message}");
-Console.WriteLine($"The runtime type of int is named: {typeof(int)}");
-Console.WriteLine($"Type of boxed integer is{(123.GetType() == typeof(int) ? "" : " not")} equal to typeof(int)");
-Console.WriteLine($"Type of boxed integer is{(123.GetType() == typeof(byte) ? "" : " not")} equal to typeof(byte)");
-Console.WriteLine($"Upper case of 'Вторник' is '{"Вторник".ToUpper()}'");
-Console.WriteLine($"Current stack frame is {new StackTrace().GetFrame(0)}");
+var input = File.ReadAllText("input.txt");
+var rules = ParseRules(input);
+
+var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+var sum1 = GetSum(['+', '*']);
+System.Console.WriteLine($"{stopWatch.Elapsed} - Part 1: {sum1}");
+var sum = GetSum(['+', '*', '|']);
+System.Console.WriteLine($"{stopWatch.Elapsed} - Part 2: {sum}");
+
+long GetSum(List<char> chars)
+{
+    var longestRule = rules.MaxBy(x => x.numbers.Count).numbers.Count;
+    var permutationDictionary = new Dictionary<int, List<string>>();
+    for (int i = 0; i < longestRule; i++)
+    {
+        permutationDictionary[i + 1] = GeneratePermutationsWithRepetition(chars.ToArray(), i + 1).ToList();
+    }
+
+    var sum = 0L;
+    foreach (var rule in rules)
+    {
+        var expectedResult = rule.result;
+        foreach (var r in permutationDictionary[rule.numbers.Count])
+        {
+            var result = rule.numbers[0];
+            for (int i = 1; i < r.Length; i++)
+            {
+                if (r[i] == '*')
+                {
+                    result *= rule.numbers[i];
+                }
+                else if (r[i] == '|')
+                {
+                    result = Concat(result, rule.numbers[i]);
+                }
+                else
+                {
+                    result += rule.numbers[i];
+                }
+                if (result > expectedResult)
+                {
+                    break;
+                }
+            }
+            if (result == expectedResult)
+            {
+                sum += result;
+                break;
+            }
+            if (result > expectedResult)
+            {
+                break;
+            }
+        }
+    }
+    return sum;
+}
+
+static long Concat(long left, long right)
+{
+    int rightLength = 10;
+    while (rightLength <= right)
+    {
+        rightLength *= 10;
+    }
+    return (left * rightLength) + right;
+}
+
+List<string> GeneratePermutationsWithRepetition(char[] characters, int length)
+{
+    var results = new List<string>();
+    GeneratePermutationsRecursive(characters, "", length, results);
+    return results;
+}
+
+void GeneratePermutationsRecursive(char[] characters, string current, int length, List<string> results)
+{
+    if (current.Length == length)
+    {
+        results.Add(current);
+        return;
+    }
+
+    foreach (var character in characters)
+    {
+        GeneratePermutationsRecursive(characters, current + character, length, results);
+    }
+}
+
+List<Test> ParseRules(string input)
+{
+    var lines = input.Split(Environment.NewLine);
+    var testList = new List<Test>();
+    foreach (var line in lines)
+    {
+        var parts = line.Split(':');
+        var result = long.Parse(parts[0]);
+        var numbers = new List<long>();
+        foreach (var number in parts[1].Split(' '))
+        {
+            if (number == string.Empty)
+                continue;
+
+            numbers.Add(long.Parse(number));
+        }
+        testList.Add(new Test(result, numbers));
+    }
+    return testList;
+}
+public record Test(long result, List<long> numbers);
