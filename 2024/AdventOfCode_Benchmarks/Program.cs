38c855b88d156390929743105c90604f2174e666
Stefan Kert <stefan.kert@fiskaltrust.eu>
2024-12-08 13:27:16 +0100

    Fixes

--- 2024/AdventOfCode_Benchmarks/Program.cs
+++ 2024/AdventOfCode_Benchmarks/Program.cs
@@ -1,30 +1,35 @@
 ﻿using AdventOfCode.Helpers;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 var summary = BenchmarkRunner.Run<AdventOfCode2024>();
 
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.NativeAot80)]
+[SimpleJob(RuntimeMoniker.Net80)]
 public class AdventOfCode2024
 {
     private AbstractSolution _day1;
-    private SolutionDay2 _day2;
-    private SolutionDay3 _day3;
-    private SolutionDay4 _day4;
-    private SolutionDay5 _day5;
-    private SolutionDay6 _day6;
-    private AbstractSolution _day7;
-    private AbstractSolution _day8;
+    //private SolutionDay2 _day2;
+    //private SolutionDay3 _day3;
+    //private SolutionDay4 _day4;
+    //private SolutionDay5 _day5;
+    //private SolutionDay6 _day6;
+    //private AbstractSolution _day7;
+    //private AbstractSolution _day8;
 
     public AdventOfCode2024()
     {
         _day1 = new SolutionDay1("C:\\GitHub\\advent-of-code\\2024\\Day1\\input.txt");
-        _day2 = new SolutionDay2("C:\\GitHub\\advent-of-code\\2024\\Day2\\input.txt");
-        _day3 = new SolutionDay3("C:\\GitHub\\advent-of-code\\2024\\Day3\\input.txt");
-        _day4 = new SolutionDay4("C:\\GitHub\\advent-of-code\\2024\\Day4\\input.txt");
-        _day5 = new SolutionDay5("C:\\GitHub\\advent-of-code\\2024\\Day5\\input.txt");
-        _day6 = new SolutionDay6("C:\\GitHub\\advent-of-code\\2024\\Day6\\input.txt");
-        _day7 = new SolutionDay7("C:\\GitHub\\advent-of-code\\2024\\Day7\\input.txt");
-        _day8 = new SolutionDay8("C:\\GitHub\\advent-of-code\\2024\\Day8\\input.txt");
+        //_day2 = new SolutionDay2("C:\\GitHub\\advent-of-code\\2024\\Day2\\input.txt");
+        //_day3 = new SolutionDay3("C:\\GitHub\\advent-of-code\\2024\\Day3\\input.txt");
+        //_day4 = new SolutionDay4("C:\\GitHub\\advent-of-code\\2024\\Day4\\input.txt");
+        //_day5 = new SolutionDay5("C:\\GitHub\\advent-of-code\\2024\\Day5\\input.txt");
+        //_day6 = new SolutionDay6("C:\\GitHub\\advent-of-code\\2024\\Day6\\input.txt");
+        //_day7 = new SolutionDay7("C:\\GitHub\\advent-of-code\\2024\\Day7\\input.txt");
+        //_day8 = new SolutionDay8("C:\\GitHub\\advent-of-code\\2024\\Day8\\input.txt");
     }
 
     [Benchmark]
@@ -73,5 +78,5 @@ public class AdventOfCode2024
     //public void Day8_PartOne() => _day8.Part1();
 
     //[Benchmark]
-    //public void Day8_PartTwo() => _day8.Part1();
+    //public void Day8_PartTwo() => _day8.Part2();
 }
