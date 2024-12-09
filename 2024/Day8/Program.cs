d6c5ea8d4bd8047ab2ff8fd5d3f9caa6b10980fa
Stefan Kert <stefan.kert@fiskaltrust.eu>
2024-12-09 20:48:45 +0100

    Finixhed day8

--- 2024/Day8/Program.cs
+++ 2024/Day8/Program.cs
@@ -1,4 +1,8 @@
 ﻿using AdventOfCode.Helpers;
+using System.Collections.Immutable;
+using System.Numerics;
+
+using Map = System.Collections.Immutable.ImmutableDictionary<System.Numerics.Complex, char>;
 
 var solution = new SolutionDay8();
 solution.PrintSolutions();
@@ -10,11 +14,83 @@ public class SolutionDay8 : AbstractSolution
 
     public override long Part1()
     {
-        return 0;
+        var map = GetMap(_input);
+        var antennaLocations = GetAntennas(map);
+        var antennaPairs = GetAntennaPairs(antennaLocations, map);
+        var data = new HashSet<Complex>();
+        foreach (var (srcAntenna, dstAntenna) in antennaPairs)
+        {
+            var dir = dstAntenna - srcAntenna;
+            var antinote = dstAntenna + dir;
+            if (map.Keys.Contains(antinote))
+            {
+                data.Add(antinote);
+            }
+        }
+        return data.ToHashSet().Count;
     }
 
     public override long Part2()
     {
-        return 0;
+        var map = GetMap(_input);
+        var antennaLocations = GetAntennas(map);
+        var antennaPairs = GetAntennaPairs(antennaLocations, map);
+        var data = new HashSet<Complex>();
+        foreach (var (srcAntenna, dstAntenna) in antennaPairs)
+        {
+            var dir = dstAntenna - srcAntenna;
+            var antinote = dstAntenna;
+            while (map.Keys.Contains(antinote))
+            {
+                data.Add(antinote);
+                antinote += dir;
+            }
+        }
+        return data.ToHashSet().Count;
+    }
+
+    List<(Complex srcAntenna, Complex dstAntenna)> GetAntennaPairs(List<Complex> antennaLocations, Map map)
+    {
+        var data = new List<(Complex srcAntenna, Complex dstAntenna)>();
+        for (int i = 0; i < antennaLocations.Count; i++)
+        {
+            for (int j = 0; j < antennaLocations.Count; j++)
+            {
+                var srcAntenna = antennaLocations[i];
+                var dstAntenna = antennaLocations[j];
+                if (map[srcAntenna] == map[dstAntenna])
+                {
+                    if (srcAntenna == dstAntenna)
+                    {
+                        continue;
+                    }
+                    data.Add((srcAntenna, dstAntenna));
+                }
+            }
+        }
+        return data;
+    }
+
+    private static List<Complex> GetAntennas(Map map)
+    {
+        var data = new List<Complex>();
+        foreach (var pos in map.Keys)
+        {
+            if (char.IsAsciiLetterOrDigit(map[pos]))
+            {
+                data.Add(pos);
+            }
+        }
+        return data;
+    }
+
+    Map GetMap(string input)
+    {
+        var map = input.Split(Environment.NewLine);
+        return (
+            from y in Enumerable.Range(0, map.Length)
+            from x in Enumerable.Range(0, map[0].Length)
+            select new KeyValuePair<Complex, char>(x - y * Complex.ImaginaryOne, map[y][x])
+        ).ToImmutableDictionary();
     }
 }
---
+++ 2024/Day9/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Helpers;
+using System.Collections.Immutable;
+using System.Numerics;
+
+
+var solution = new SolutionDay9();
+solution.PrintSolutions();
+
+public class SolutionDay9 : AbstractSolution
+{
+    public SolutionDay9() : base() { }
+    public SolutionDay9(string filepath) : base(filepath) { }
+
+    public override long Part1()
+    {
+        return -1;
+    }
+
+    public override long Part2()
+    {
+        return -1;
+    }
+
+}
