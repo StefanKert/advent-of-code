3d1c7b5315ecdba07012dd326b8a0dcbba4df365
Stefan Kert <stefan.kert@fiskaltrust.eu>
2024-12-13 21:04:00 +0100

    Fix

--- 2024/Day11/Program.cs
+++ 2024/Day11/Program.cs
@@ -1,164 +1,62 @@
 ﻿using AdventOfCode.Helpers;
+using System.Globalization;
+using Cache = System.Collections.Concurrent.ConcurrentDictionary<(long, int), long>;
 
-var solution = new SolutionDay9("input.txt");
+var solution = new SolutionDay11();
 solution.PrintSolutions();
 
-//Console.WriteLine($"Part 2: {solution.Part2()}");
-
-public class SolutionDay9 : AbstractSolution
+public class SolutionDay11 : AbstractSolution
 {
-    public const char FREE = '.';
-
-    public SolutionDay9() : base() { }
-    public SolutionDay9(string filepath) : base(filepath) { }
+    public SolutionDay11() : base() { }
+    public SolutionDay11(string filepath) : base(filepath) { }
 
     public override long Part1()
     {
-        var dataAtDisk = GetDiskLayout();
-        Compress(dataAtDisk);
-        return GetCheckSum(dataAtDisk);
+        return CountStones(25);
     }
 
     public override long Part2()
     {
-        var dataAtDisk = GetDiskLayout();
-        CompressOnlyFit(dataAtDisk);
-        return GetCheckSum(dataAtDisk);
+        return CountStones(75);
     }
 
-    private static void PrintDataAtDisk(List<File> dataAtDisk)
+    public long CountStones(int blinks)
     {
-        return;
-        foreach (var file in dataAtDisk)
+        var cache = new Cache();
+        var sum = 0L;
+        foreach (var stone in GetStones(_input))
         {
-            for (int i = 0; i < file.Size; i++)
-            {
-                Console.Write(file.Id);
-            }
-            for (int i = 0; i < file.Free; i++)
-            {
-                Console.Write(FREE);
-            }
+            sum += Blink(stone, blinks, cache);
         }
-        Console.WriteLine();
+        return sum;
     }
 
-    private static long GetCheckSum(List<File> files)
+    public long Blink(long stone, int blinks, Cache cache)
     {
-        var checksum = 0L;
-        var position = 0;
-        for (int i = 0; i < files.Count; i++)
+        return cache.GetOrAdd((stone, blinks), key =>
         {
-            for (int j = 0; j < files[i].Size; j++)
+            if (key.Item2 == 0)
             {
-                checksum += position * long.Parse(files[i].Id);
-                position++;
+                return 1;
             }
-            for (int j = 0; j < files[i].Free; j++)
+            else if (key.Item1 == 0)
             {
-                position++;
+                return Blink(1, blinks - 1, cache);
             }
-        }
-        return checksum;
+            else if (key.Item1.ToString().Length % 2 == 0)
+            {
+                var stoneData = stone.ToString();
+                return Blink(long.Parse(stoneData[0..(stoneData.Length / 2)]), blinks - 1, cache) + Blink(long.Parse(stoneData[(stoneData.Length / 2)..]), blinks - 1, cache);
+            }
+            else
+            {
+                return Blink(stone * 2024, blinks - 1, cache);
+            }
+        });
     }
 
-    private static void Compress(List<File> dataAtDisk)
+    public List<long> GetStones(string input)
     {
-        while (true)
-        {
-            var lastFreeFileIndex = dataAtDisk.FindIndex(f => f.Free > 0);
-            var lastSizeFileIndex = dataAtDisk.FindLastIndex(f => f.Size > 0);
-            if (lastFreeFileIndex >= lastSizeFileIndex)
-            {
-                break;
-            }
-
-            var lastFile = dataAtDisk[lastSizeFileIndex];
-            for (int i = 0; i < dataAtDisk.Count; i++)
-            {
-                if (dataAtDisk[i].Free > 0)
-                {
-                    if (dataAtDisk[i].Free >= lastFile.Size)
-                    {
-                        dataAtDisk.Insert(i + 1, new File
-                        {
-                            Id = lastFile.Id,
-                            Size = lastFile.Size,
-                            Free = dataAtDisk[i].Free - lastFile.Size
-                        });
-                        dataAtDisk[i].Free = 0;
-                        lastFile.Free += lastFile.Size;
-                        lastFile.Size = 0;
-                        break;
-                    }
-                    else
-                    {
-                        dataAtDisk.Insert(i + 1, new File
-                        {
-                            Id = lastFile.Id,
-                            Size = dataAtDisk[i].Free,
-                            Free = 0
-                        });
-                        lastFile.Size -= dataAtDisk[i].Free;
-                        lastFile.Free += dataAtDisk[i].Free;
-                        dataAtDisk[i].Free = 0;
-                    }
-                }
-            }
-            PrintDataAtDisk(dataAtDisk);
-        }
-    }
-
-    private static void CompressOnlyFit(List<File> dataAtDisk)
-    {
-        var fileToMove = dataAtDisk.Count - 1;
-        for (int i = fileToMove; i >= 0; i--)
-        {
-            var activeFile = dataAtDisk[i];
-            for (int j = 0; j < i; j++)
-            {
-                if (dataAtDisk[j].Free >= activeFile.Size)
-                {
-                    dataAtDisk.Insert(j + 1, new File
-                    {
-                        Id = activeFile.Id,
-                        Size = activeFile.Size,
-                        Free = dataAtDisk[j].Free - activeFile.Size
-                    });
-                    dataAtDisk[j].Free = 0;
-                    activeFile.Free += activeFile.Size;
-                    activeFile.Size = 0;
-                    break;
-                }
-            }
-            PrintDataAtDisk(dataAtDisk);
-        }
-    }
-
-    private List<File> GetDiskLayout()
-    {
-        var files = new List<File>();
-        var fileId = 0;
-        for (int i = 0; i < _input.Length; i++)
-        {
-            var file = new File
-            {
-                Id = fileId++.ToString(),
-                Size = long.Parse(_input[i].ToString()),
-            };
-            if (++i < _input.Length)
-            {
-                file.Free = long.Parse(_input[i].ToString());
-            }
-            files.Add(file);
-        }
-        return files;
-    }
-
-    public record File
-    {
-        public string Id { get; set; }
-        public long Size { get; set; }
-        public long Free { get; set; }
+        return input.Split(' ').Select(long.Parse).ToList();
     }
 }
--- 2024/Day12/Program.cs
+++ 2024/Day12/Program.cs
@@ -1,164 +1,20 @@
 ﻿using AdventOfCode.Helpers;
 
-var solution = new SolutionDay9("input.txt");
+var solution = new SolutionDay12("input.txt");
 solution.PrintSolutions();
 
-//Console.WriteLine($"Part 2: {solution.Part2()}");
-
-public class SolutionDay9 : AbstractSolution
+public class SolutionDay12 : AbstractSolution
 {
-    public const char FREE = '.';
-
-    public SolutionDay9() : base() { }
-    public SolutionDay9(string filepath) : base(filepath) { }
+    public SolutionDay12() : base() { }
+    public SolutionDay12(string filepath) : base(filepath) { }
 
     public override long Part1()
     {
-        var dataAtDisk = GetDiskLayout();
-        Compress(dataAtDisk);
-        return GetCheckSum(dataAtDisk);
+        return 0;
     }
 
     public override long Part2()
     {
-        var dataAtDisk = GetDiskLayout();
-        CompressOnlyFit(dataAtDisk);
-        return GetCheckSum(dataAtDisk);
-    }
-
-    private static void PrintDataAtDisk(List<File> dataAtDisk)
-    {
-        return;
-        foreach (var file in dataAtDisk)
-        {
-            for (int i = 0; i < file.Size; i++)
-            {
-                Console.Write(file.Id);
-            }
-            for (int i = 0; i < file.Free; i++)
-            {
-                Console.Write(FREE);
-            }
-        }
-        Console.WriteLine();
-    }
-
-    private static long GetCheckSum(List<File> files)
-    {
-        var checksum = 0L;
-        var position = 0;
-        for (int i = 0; i < files.Count; i++)
-        {
-            for (int j = 0; j < files[i].Size; j++)
-            {
-                checksum += position * long.Parse(files[i].Id);
-                position++;
-            }
-            for (int j = 0; j < files[i].Free; j++)
-            {
-                position++;
-            }
-        }
-        return checksum;
-    }
-
-    private static void Compress(List<File> dataAtDisk)
-    {
-        while (true)
-        {
-            var lastFreeFileIndex = dataAtDisk.FindIndex(f => f.Free > 0);
-            var lastSizeFileIndex = dataAtDisk.FindLastIndex(f => f.Size > 0);
-            if (lastFreeFileIndex >= lastSizeFileIndex)
-            {
-                break;
-            }
-
-            var lastFile = dataAtDisk[lastSizeFileIndex];
-            for (int i = 0; i < dataAtDisk.Count; i++)
-            {
-                if (dataAtDisk[i].Free > 0)
-                {
-                    if (dataAtDisk[i].Free >= lastFile.Size)
-                    {
-                        dataAtDisk.Insert(i + 1, new File
-                        {
-                            Id = lastFile.Id,
-                            Size = lastFile.Size,
-                            Free = dataAtDisk[i].Free - lastFile.Size
-                        });
-                        dataAtDisk[i].Free = 0;
-                        lastFile.Free += lastFile.Size;
-                        lastFile.Size = 0;
-                        break;
-                    }
-                    else
-                    {
-                        dataAtDisk.Insert(i + 1, new File
-                        {
-                            Id = lastFile.Id,
-                            Size = dataAtDisk[i].Free,
-                            Free = 0
-                        });
-                        lastFile.Size -= dataAtDisk[i].Free;
-                        lastFile.Free += dataAtDisk[i].Free;
-                        dataAtDisk[i].Free = 0;
-                    }
-                }
-            }
-            PrintDataAtDisk(dataAtDisk);
-        }
-    }
-
-    private static void CompressOnlyFit(List<File> dataAtDisk)
-    {
-        var fileToMove = dataAtDisk.Count - 1;
-        for (int i = fileToMove; i >= 0; i--)
-        {
-            var activeFile = dataAtDisk[i];
-            for (int j = 0; j < i; j++)
-            {
-                if (dataAtDisk[j].Free >= activeFile.Size)
-                {
-                    dataAtDisk.Insert(j + 1, new File
-                    {
-                        Id = activeFile.Id,
-                        Size = activeFile.Size,
-                        Free = dataAtDisk[j].Free - activeFile.Size
-                    });
-                    dataAtDisk[j].Free = 0;
-                    activeFile.Free += activeFile.Size;
-                    activeFile.Size = 0;
-                    break;
-                }
-            }
-            PrintDataAtDisk(dataAtDisk);
-        }
-    }
-
-    private List<File> GetDiskLayout()
-    {
-        var files = new List<File>();
-        var fileId = 0;
-        for (int i = 0; i < _input.Length; i++)
-        {
-            var file = new File
-            {
-                Id = fileId++.ToString(),
-                Size = long.Parse(_input[i].ToString()),
-            };
-            if (++i < _input.Length)
-            {
-                file.Free = long.Parse(_input[i].ToString());
-            }
-            files.Add(file);
-        }
-        return files;
-    }
-
-    public record File
-    {
-        public string Id { get; set; }
-        public long Size { get; set; }
-        public long Free { get; set; }
+        return 0;
     }
 }
