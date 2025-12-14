77ffb247667e04d2c77ee10b4c791a4a6e46f3ca
Stefan Kert <stefan.kert@fiskaltrust.eu>
2025-12-14 11:56:06 +0100

    Wrapped up day8

--- 2025/src/Day8/Program.cs
+++ 2025/src/Day8/Program.cs
@@ -1,34 +1,21 @@
-﻿using System.Linq;
-
-var junctionBoxes = File.ReadAllLines("input.txt").Select(x => (x: long.Parse(x.Split(",")[0]), y: long.Parse(x.Split(",")[1]), z: long.Parse(x.Split(",")[2]))).ToList();
+﻿var junctionBoxes = File.ReadAllLines("input.txt").Select(x => (x: long.Parse(x.Split(",")[0]), y: long.Parse(x.Split(",")[1]), z: long.Parse(x.Split(",")[2]))).ToList();
 int iterations = 1000;
+var distances = CalculateDistances(junctionBoxes);
 
-List<(double distance, (long x, long y, long z) point1, (long x, long y, long z) point2)> distances = new();
-
-var seen = new HashSet<(int, int)>();
-
-for (int i = 0; i < junctionBoxes.Count; i++)
-{
-    for (int j = 0; j < junctionBoxes.Count; j++)
-    {
-        if (i == j) continue;
-
-        var a = Math.Min(i, j);
-        var b = Math.Max(i, j);
-
-        if (!seen.Add((a, b))) continue;
-
-        var d = Distance3D(junctionBoxes[i], junctionBoxes[j]);
-        distances.Add((d, junctionBoxes[i], junctionBoxes[j]));
-    }
-}
-
+(double distance, (long x, long y, long z) point1, (long x, long y, long z) point2) lastPoint = (0, (0, 0, 0), (0, 0, 0));
 var circuits = new List<List<(long x, long y, long z)>>();
-foreach (var point in distances.OrderBy(x => x.distance).Take(iterations))
+
+foreach (var point in distances.OrderBy(x => x.distance))
 {
     if (circuits.Any(x => x.Contains(point.point1) && x.Contains(point.point2)))
     {
         continue;
+    }
+
+    var tempLastPoint = lastPoint;
+    if (junctionBoxes.Count + circuits.Count == 2)
+    {
+        lastPoint = point;
     }
 
     if (circuits.Any(x => x.Contains(point.point1)))
@@ -71,10 +58,16 @@ foreach (var point in distances.OrderBy(x => x.distance).Take(iterations))
         junctionBoxes.Remove(point.point2);
         circuits.Add(new List<(long x, long y, long z)>() { point.point1, point.point2 });
     }
+
+    if (junctionBoxes.Count == 0 && circuits.Count == 1)
+    {
+        // in this case we reached the last point
+        lastPoint = tempLastPoint;
+    }
 }
 
 var result = circuits.OrderByDescending(x => x.Count).Take(3).Select(x => x.Count).Aggregate(1L, (a, b) => a * b);
-Console.WriteLine(result);
+Console.WriteLine("Distance Last point: " + lastPoint.point1.x * lastPoint.point2.x);
 
 static double Distance3D(
     (long x, long y, long z) point1,
@@ -85,4 +78,29 @@ static double Distance3D(
         Math.Pow(point2.y - point1.y, 2) +
         Math.Pow(point2.z - point1.z, 2)
     );
+}
+
+static List<(double distance, (long x, long y, long z) point1, (long x, long y, long z) point2)> CalculateDistances(List<(long x, long y, long z)> junctionBoxes)
+{
+    List<(double distance, (long x, long y, long z) point1, (long x, long y, long z) point2)> distances = new();
+
+    var seen = new HashSet<(int, int)>();
+
+    for (int i = 0; i < junctionBoxes.Count; i++)
+    {
+        for (int j = 0; j < junctionBoxes.Count; j++)
+        {
+            if (i == j) continue;
+
+            var a = Math.Min(i, j);
+            var b = Math.Max(i, j);
+
+            if (!seen.Add((a, b))) continue;
+
+            var d = Distance3D(junctionBoxes[i], junctionBoxes[j]);
+            distances.Add((d, junctionBoxes[i], junctionBoxes[j]));
+        }
+    }
+
+    return distances;
 }
